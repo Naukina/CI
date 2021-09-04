cbddecba63a179229496edc94b510d54f731716d
Naukina Aidana <naukina_aidana@mail.ru>
2021-09-04 16:28:51 +0600

    test added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+  @Test
+  void findMaxWithAnotherArgs() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 8, 6, 11, 11, 12};
+    long expected = 25;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
 }
